06840ba1e63d64b88aa905a198009b0d13b43736
Student16 <student16@itpark.ru>
2018-03-30 20:38:40 +0300

    work

---
+++ ClassWorks/Task 3/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char a = 'b';
+        System.out.println((char)(a +'Z'-'z'));
+    }
+}
---
+++ ClassWorks/Task 4/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char text[] = "hello how are l you".toCharArray();
+        int chars[] = new int[27];
+
+        // chars[0] - количество букв а
+        // chars[1] - количество букв b
+        // chars[2] - колиечтво c
+
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] != ' ') {
+                int charIndex = text[i] - 'a';
+                chars[charIndex]++;
+            }
+        }
+
+        int maxValue = chars[0];
+        int letterIndex = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] > maxValue) {
+                letterIndex = i;
+                maxValue = chars[i];
+            }
+        }
+
+        System.out.println((char)(letterIndex + 'a'));
+
+        int i = 0;
+
+    }
+    }
+
